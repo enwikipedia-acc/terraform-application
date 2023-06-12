85308bbd1ace629c056f080acf7812faf906f326
Simon Walker <github@stwalkerster.co.uk>
2023-06-12 23:11:25 +0100

    deploy db7 as bookworm (#3)
    * deploy db7 as bookworm
    
    * SGs, disk, disk attachment

--- legacy.tf
+++ legacy.tf
@@ -11,3 +11,46 @@ resource "openstack_dns_recordset_v2" "legacy_prod_db" {
   records = [data.openstack_compute_instance_v2.accounts-db6.access_ip_v4]
   ttl     = 180
 }
+
+data "openstack_images_image_v2" "legacy_image" {
+  most_recent = true
+  name        = "debian-12.0-bookworm"
+}
+
+resource "openstack_compute_instance_v2" "legacy_db7" {
+  name            = "${var.resource_prefix}-db7"
+  image_id        = data.openstack_images_image_v2.legacy_image.id
+  flavor_id       = data.openstack_compute_flavor_v2.small.id
+  user_data       = null
+  security_groups = [
+    openstack_networking_secgroup_v2.default.name,
+    openstack_networking_secgroup_v2.db.name,
+  ]
+
+  metadata = {
+    terraform   = "Yes"
+    environment = "Legacy"
+  }
+
+  network {
+    uuid = data.openstack_networking_network_v2.network.id
+  }
+
+  lifecycle {
+    ignore_changes = [
+      image_id
+    ]
+  }
+}
+
+resource "openstack_blockstorage_volume_v3" "legacy_db7" {
+  name        = "${var.resource_prefix}-db7"
+  description = "Application database; managed by Terraform"
+  size        = 5
+}
+
+resource "openstack_compute_volume_attach_v2" "legacy_db7" {
+  instance_id = openstack_compute_instance_v2.legacy_db7.id
+  volume_id   = openstack_blockstorage_volume_v3.legacy_db7.id
+  device      = "/dev/sdb"
+}
