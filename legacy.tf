3266ad6721cb22f5f93f64a7513a4d231c39fd42
Simon Walker <github@stwalkerster.co.uk>
2023-07-02 01:59:16 +0100

    Import legacy db6 resources

--- legacy.tf
+++ legacy.tf
@@ -1,8 +1,45 @@
 # This file contains a bridge to the legacy non-managed infrastructure
 
-data "openstack_compute_instance_v2" "accounts-db6" {
-  id = "c07b5734-221d-4056-a5c2-fe995770fcf0"
+resource "openstack_compute_instance_v2" "legacy_db6" {
+  name            = "${var.resource_prefix}-db6"
+  image_id        = data.openstack_images_image_v2.legacy_image.id
+  flavor_id       = data.openstack_compute_flavor_v2.small.id
+  user_data       = null
+  power_state     = "shutoff"
+
+  security_groups = [
+    openstack_networking_secgroup_v2.default.name,
+    openstack_networking_secgroup_v2.db.name,
+    "database",
+    "default"
+  ]
+
+  metadata = {
+    terraform   = "Yes"
+    environment = "Legacy"
+  }
+
+  network {
+    uuid = data.openstack_networking_network_v2.network.id
+  }
+
+  lifecycle {
+    ignore_changes = [
+      image_id
+    ]
+  }
 }
+
+resource "openstack_blockstorage_volume_v3" "legacy_db6" {
+  name        = "app-db"
+  size        = 5
+}
+
+resource "openstack_blockstorage_volume_v3" "legacy_db6_backup" {
+  name        = "app-dbbackup"
+  size        = 2
+}
+
 
 resource "openstack_dns_recordset_v2" "legacy_prod_db" {
   name    = "db-legacy.${data.openstack_dns_zone_v2.rootzone.name}"
