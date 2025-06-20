c99cc9cef8bfb8606873037acca363eac15fab27
Simon Walker <github@stwalkerster.co.uk>
2025-06-20 19:43:42 +0100

    Create appserrver7

--- data.tf
+++ data.tf
@@ -9,10 +9,26 @@ data "openstack_networking_network_v2" "network" {
   name     = "VLAN/legacy"
 }
 
+data "openstack_networking_network_v2" "dualstack" {
+  external = false
+  name     = "VXLAN/IPv6-dualstack"
+}
+
 data "openstack_networking_secgroup_v2" "default" {
   name = "default"
 }
 
 data "openstack_dns_zone_v2" "rootzone" {
   name = var.dns_zone
+}
+
+
+data "openstack_images_image_v2" "bullseye" {
+  most_recent = true
+  name        = "debian-11.0-bullseye"
+}
+
+data "openstack_images_image_v2" "bookworm" {
+  most_recent = true
+  name        = "debian-12.0-bookworm"
 }
