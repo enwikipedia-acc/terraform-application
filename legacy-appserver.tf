13f201a5bc51478a2d76fc8e6313cc91121b59c4
Simon Walker <github@stwalkerster.co.uk>
2025-06-20 19:43:42 +0100

    Create appserrver7

--- legacy-appserver.tf
+++ legacy-appserver.tf
@@ -53,3 +53,24 @@ resource "openstack_compute_volume_attach_v2" "legacy_app6" {
 data "openstack_networking_secgroup_v2" "legacy_web" {
   name = "web"
 }
+
+resource "openstack_compute_instance_v2" "legacy_app7" {
+  name            = "${var.resource_prefix}-appserver7"
+  image_id        = data.openstack_images_image_v2.bullseye.id
+  flavor_id       = data.openstack_compute_flavor_v2.small.id
+  user_data       = null
+  security_groups = [
+    openstack_networking_secgroup_v2.default.name,
+    openstack_networking_secgroup_v2.app.name,
+  ]
+
+  network {
+    uuid = data.openstack_networking_network_v2.dualstack.id
+  }
+
+  lifecycle {
+    ignore_changes = [
+      image_id
+    ]
+  }
+}
