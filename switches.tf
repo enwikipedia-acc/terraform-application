107aca6c901ee87713e665c1501c59c5e3af148c
Simon Walker <github@stwalkerster.co.uk>
2025-06-20 23:07:46 +0100

    Point proxies to new instance

--- switches.tf
+++ switches.tf
@@ -1,4 +1,4 @@
 locals {
-  production_app_instance_ip4 = openstack_compute_instance_v2.legacy_app6.access_ip_v4
+  production_app_instance_ip4 = openstack_compute_instance_v2.legacy_app7.access_ip_v4
   production_db_instance_ip4 = openstack_compute_instance_v2.legacy_db7.access_ip_v4
 }
