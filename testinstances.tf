a4178a95532902370cade3c344731b7df7695aee
Simon Walker <github@stwalkerster.co.uk>
2025-06-28 11:48:14 +0100

    Create a couple of test instances for NTP diagnostics

---
+++ testinstances.tf
@@ -0,0 +1,41 @@
+resource "openstack_compute_instance_v2" "testinstance11" {
+  name            = "${var.resource_prefix}-test11"
+  image_id        = data.openstack_images_image_v2.bullseye.id
+  flavor_id       = data.openstack_compute_flavor_v2.small.id
+  user_data       = null
+  security_groups = [
+    openstack_networking_secgroup_v2.default.name,
+    openstack_networking_secgroup_v2.app.name
+  ]
+
+  network {
+    uuid = data.openstack_networking_network_v2.dualstack.id
+  }
+
+  lifecycle {
+    ignore_changes = [
+      image_id
+    ]
+  }
+}
+
+resource "openstack_compute_instance_v2" "testinstance12" {
+  name            = "${var.resource_prefix}-test12"
+  image_id        = data.openstack_images_image_v2.bookworm.id
+  flavor_id       = data.openstack_compute_flavor_v2.small.id
+  user_data       = null
+  security_groups = [
+    openstack_networking_secgroup_v2.default.name,
+    openstack_networking_secgroup_v2.app.name
+  ]
+
+  network {
+    uuid = data.openstack_networking_network_v2.dualstack.id
+  }
+
+  lifecycle {
+    ignore_changes = [
+      image_id
+    ]
+  }
+}
