171d691786f6bf5f89af82d9032c214b9fdedf6c
Simon Walker <github@stwalkerster.co.uk>
2025-06-28 17:04:54 +0100

    Recreate test instances

--- testinstances.tf
+++ testinstances.tf
@@ -1,85 +1,62 @@
-# resource "openstack_compute_instance_v2" "testinstance11" {
-#   name            = "${var.resource_prefix}-test11"
-#   image_id        = data.openstack_images_image_v2.bullseye.id
-#   flavor_id       = data.openstack_compute_flavor_v2.small.id
-#   user_data       = null
-#   security_groups = [
-#     openstack_networking_secgroup_v2.default.name,
-#     openstack_networking_secgroup_v2.app.name,
-#   ]
+resource "openstack_compute_instance_v2" "testinstance11" {
+  name            = "${var.resource_prefix}-testinstance11"
+  image_id        = data.openstack_images_image_v2.bullseye.id
+  flavor_id       = data.openstack_compute_flavor_v2.small.id
+  user_data       = null
+  security_groups = [
+    openstack_networking_secgroup_v2.default.name,
+    openstack_networking_secgroup_v2.app.name,
+  ]
 
-#   network {
-#     uuid = data.openstack_networking_network_v2.dualstack.id
-#   }
+  network {
+    uuid = data.openstack_networking_network_v2.dualstack.id
+  }
 
-#   lifecycle {
-#     ignore_changes = [
-#       image_id
-#     ]
-#   }
-# }
+  lifecycle {
+    ignore_changes = [
+      image_id
+    ]
+  }
+}
 
-# resource "openstack_compute_instance_v2" "testinstance11_legacy" {
-#   name            = "${var.resource_prefix}-test11-legacy"
-#   image_id        = data.openstack_images_image_v2.bullseye.id
-#   flavor_id       = data.openstack_compute_flavor_v2.small.id
-#   user_data       = null
-#   security_groups = [
-#     openstack_networking_secgroup_v2.default.name,
-#     openstack_networking_secgroup_v2.app.name,
-#   ]
+resource "openstack_compute_instance_v2" "testinstance11_legacy" {
+  name            = "${var.resource_prefix}-testinstance11-legacy"
+  image_id        = data.openstack_images_image_v2.bullseye.id
+  flavor_id       = data.openstack_compute_flavor_v2.small.id
+  user_data       = null
+  security_groups = [
+    openstack_networking_secgroup_v2.default.name,
+    openstack_networking_secgroup_v2.app.name,
+  ]
 
-#   network {
-#     uuid = data.openstack_networking_network_v2.legacy.id
-#   }
+  network {
+    uuid = data.openstack_networking_network_v2.legacy.id
+  }
 
-#   lifecycle {
-#     ignore_changes = [
-#       image_id
-#     ]
-#   }
-# }
+  lifecycle {
+    ignore_changes = [
+      image_id
+    ]
+  }
+}
 
-# resource "openstack_compute_instance_v2" "testinstance12" {
-#   name            = "${var.resource_prefix}-test12"
-#   image_id        = data.openstack_images_image_v2.bookworm.id
-#   flavor_id       = data.openstack_compute_flavor_v2.small.id
-#   user_data       = null
-#   security_groups = [
-#     openstack_networking_secgroup_v2.default.name,
-#     openstack_networking_secgroup_v2.app.name,
-#   ]
+resource "openstack_compute_instance_v2" "testinstance12" {
+  name            = "${var.resource_prefix}-testinstance12"
+  image_id        = data.openstack_images_image_v2.bookworm.id
+  flavor_id       = data.openstack_compute_flavor_v2.small.id
+  user_data       = null
+  security_groups = [
+    openstack_networking_secgroup_v2.default.name,
+    openstack_networking_secgroup_v2.app.name,
+  ]
 
-#   network {
-#     uuid = data.openstack_networking_network_v2.dualstack.id
-#   }
+  network {
+    uuid = data.openstack_networking_network_v2.dualstack.id
+  }
 
-#   lifecycle {
-#     ignore_changes = [
-#       image_id
-#     ]
-#   }
-# }
-
-# module "dns_test11" {
-#   source = "./modules/dns"
-
-#   access_ip_v4 = openstack_compute_instance_v2.testinstance11.access_ip_v4
-#   access_ip_v6 = openstack_compute_instance_v2.testinstance11.access_ip_v6
-#   name         = "test11" 
-# }
-
-# module "dns_test11_legacy" {
-#   source = "./modules/dns"
-
-#   access_ip_v4 = openstack_compute_instance_v2.testinstance11_legacy.access_ip_v4
-#   name         = "test11-legacy" 
-# }
-
-# module "dns_test12" {
-#   source = "./modules/dns"
-
-#   access_ip_v4 = openstack_compute_instance_v2.testinstance12.access_ip_v4
-#   access_ip_v6 = openstack_compute_instance_v2.testinstance12.access_ip_v6
-#   name         = "test12" 
-# }
+  lifecycle {
+    ignore_changes = [
+      image_id
+    ]
+  }
+}
